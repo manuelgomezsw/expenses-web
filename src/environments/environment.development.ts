69ea820755119a225cd1e655f9aaf310304e0ef4
Jose M. Gomez L. <manuelgomezjp@gmail.com>
2025-10-04 12:45:19 -0500

    feat: actualizar estilos y funcionalidad en componentes de gastos diarios y fijos
    🔧 **Mejoras en la Interfaz:**
    - ✅ **Estilos CSS** - Se añadieron nuevos estilos para los encabezados colapsables, mejorando la experiencia de usuario al interactuar con los componentes.
    - ✅ **Lógica de Interacción** - Se implementaron eventos de `touchstart` en los encabezados colapsables para mejorar la usabilidad en dispositivos táctiles.
    
    📱 **Experiencia de Usuario:**
    - ✅ **Interacción Mejorada** - Los usuarios ahora pueden colapsar y expandir secciones de gastos de manera más intuitiva, facilitando la gestión de sus finanzas.
    
    Con estas mejoras, la gestión de gastos es más eficiente y proporciona una mejor experiencia al usuario. 🎯✨

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,6 @@
 export const environment = {
     production: false,
     titleWebSite: 'Control Financiero Personal (Dev)',
-    paymentsTypeUrl: 'http://localhost:8080/payments/type',
-    pocketsUrl: 'http://localhost:8080/pockets',
-    cyclesUrl: 'http://localhost:8080/cycles',
-    expensesUrl: 'http://localhost:8080/expenses',
-    budgetsUrl: 'http://localhost:8080/budgets',
-    conceptsUrl: 'http://localhost:8080/concepts',
     incomeUrl: 'http://localhost:8080/api/config/income',
     fixedExpensesUrl: 'http://localhost:8080/api/fixed-expenses',
     pocketsApiUrl: 'http://localhost:8080/api/config/pockets',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
     production: true,
     titleWebSite: 'Control Financiero Personal',
-    paymentsTypeUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/payments/type',
-    pocketsUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/pockets',
-    cyclesUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/cycles',
-    expensesUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/expenses',
-    budgetsUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/budgets',
-    conceptsUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/concepts',
     incomeUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/api/config/income',
     fixedExpensesUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/api/fixed-expenses',
     pocketsApiUrl: 'https://expenses-api-dot-quotes-api-100.ue.r.appspot.com/api/config/pockets',
